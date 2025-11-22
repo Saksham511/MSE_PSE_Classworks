2271453a2a6ced2f1d05c4d00f76e8b6d17e49ee
Saksham511 <sakshamshrestha511@gmail.com>
2025-11-23 01:34:09 +0545

    merge

---
+++ Activity.py
@@ -0,0 +1,9 @@
+
+Name = "John Doe"   #String
+Age = 28            #Int
+Skills = ["Python", "SQL", "Power BI"]  #List
+Education = ("BSc Computer Science", 2020)  #Tuple
+Contact_details = {"email": "joedoe@gmail.com", "phone": 32435235}  #Dictionary
+Certifications = {"Azure", "AWS", "Azure"}   #Set
+last = list(Certifications)[-1]   # pick last element
+Certifications.remove(last)  #removing last element
